7becdc938a2bec718b286e4c63202779618b6f12
pratibha_hankare <pratibha1hankare@gmail.com>
2020-04-07 11:23:42 +0530

    the handlebars templates are working

--- 03_Addind_TemplateEngin_PUG/routes/admin.js
+++ 03_Addind_TemplateEngin_PUG/routes/admin.js
@@ -8,10 +8,15 @@ const router = express.Router();
 
 const products = [];
 
-// /admin/add-product => GET
-router.get('/add-product', (req, res, next) => {
+// /admin/add-product => GET // for .pug
+/*router.get('/add-product', (req, res, next) => {
   res.render('add-product', { pageTitle: 'Add Product', path: '/admin/add-product' });
+});*/
+
+router.get('/add-product', (req, res, next) => {
+  res.render('add-product', { pageTitle: 'Add Product', path: '/admin/add-product',formCSS:true,productCSS:true,activeAddProduct:true });
 });
+
 
 // /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
--- 03_Addind_TemplateEngin_PUG/routes/shop.js
+++ 03_Addind_TemplateEngin_PUG/routes/shop.js
@@ -7,9 +7,17 @@ const adminData = require('./admin');
 
 const router = express.Router();
 
+/** route for pug 
 router.get('/', (req, res, next) => {
   const products = adminData.products;
   res.render('shop', {prods: products, pageTitle: 'Shop', path: '/'});
+});
+*/
+
+/** route for HandleBar */
+router.get('/', (req, res, next) => {
+  const products = adminData.products;
+  res.render('shop', {prods: products, pageTitle: 'Shop', path: '/',hasProducts:products.length > 0,activeShop:true,productCSS:true,formCSS:true});
 });
 
 module.exports = router;
