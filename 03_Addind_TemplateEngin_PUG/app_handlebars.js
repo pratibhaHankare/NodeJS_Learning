f73d0230b2dae56bbf6ba8db0883296b0fd14326
pratibha_hankare <pratibha1hankare@gmail.com>
2020-04-09 10:46:21 +0530

    the ejs is done

--- 03_Addind_TemplateEngin_PUG/app_handlebars.js
+++ 03_Addind_TemplateEngin_PUG/app_handlebars.js
@@ -2,10 +2,12 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const expressHbs = require('express-handlebars');
 
 const app = express();
+app.engine('hbs',expressHbs({layoutsDir:'./views/layout/',defaultLayout:'main-layout',extname:'hbs'}));
 
-app.set('view engine', 'ejs');
+app.set('view engine', 'hbs');
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');
